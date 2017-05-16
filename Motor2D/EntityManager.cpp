25e7a66ffdd4d1713175e49889a6f8039400e46d
rodrigodpl <rodrigodpl97@gmail.com>
2017-05-16 02:06:06 +0200

    Not collider fix

--- Motor2D/EntityManager.cpp
+++ Motor2D/EntityManager.cpp
@@ -656,11 +656,11 @@ void EntityManager::DeleteEntity(Entity* entity)
 				}
 
 				App->collision->DeleteCollider(unit->collider);
-				//unit->collider = nullptr;
+				unit->collider = nullptr;
 				App->collision->DeleteCollider(unit->range);
-				//unit->range = nullptr;
+				unit->range = nullptr;
 				App->collision->DeleteCollider(unit->los);
-				//unit->los = nullptr;
+				unit->los = nullptr;
 
 				if(unit->squad)
 					unit->squad->Deassign(unit);
@@ -676,11 +676,11 @@ void EntityManager::DeleteEntity(Entity* entity)
 
 				building = (Building*)entity;
 				App->collision->DeleteCollider(building->collider);
-				//building->collider = nullptr;
+				building->collider = nullptr;
 				App->collision->DeleteCollider(building->range);
-				//building->range = nullptr;
+				building->range = nullptr;
 				App->collision->DeleteCollider(building->los);
-				//building->los = nullptr;
+				building->los = nullptr;
 
 				break;
 
@@ -730,7 +730,7 @@ void EntityManager::DeleteEntity(Entity* entity)
 				
 
 				App->collision->DeleteCollider(entity->collider);
-				//entity->collider = nullptr;
+				entity->collider = nullptr;
 
 				break;
 			}
